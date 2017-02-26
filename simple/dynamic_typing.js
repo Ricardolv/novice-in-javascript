79a916f00b6b2a50b7437c33d66361eeaef13b83
Ricardolv <richardluizv@gmail.com>
2017-02-25 22:55:42 -0300

    tipagem dinamica

---
+++ simple/dynamic_typing.js
@@ -0,0 +1,16 @@
+var idade = 25;
+console.log(typeof idade);
+console.log(idade);
+
+idade += ' anos';
+
+console.log(typeof idade);
+console.log(idade);
+
+idade = false
+console.log(typeof idade);
+console.log(idade);
+
+// Tipagem Dinamica
+// vatagens: flexibilidade, em sistemas mais complexos  
+// desvantagens: pode perder o controle tranzendo uma certa inseguranca
