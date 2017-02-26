ae08031f071e677e62fb560808759d9a36d7ed60
Ricardolv <richardluizv@gmail.com>
2017-02-25 22:39:54 -0300

    variavel e hoisting

---
+++ simple/var_hoisting.js
@@ -0,0 +1,23 @@
+var percentualImposto = 0.15;
+valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto);
+
+/*
+	var percentualImposto, valor, percentualImposto;
+	percentualImposto = 0.15;
+	valor = 100;
+	totalImposto = valor * percentualImposto;
+
+	console.log('Total imposto a pagar', totalImposto);
+
+- HOISTING
+  O hoisting, traduziria dessa forma, 
+  ele pega todas as declaroes de variaveis e coloca para cima como na linha 8, 
+  apenas a declaracao da variavel.  
+
+
+  O hoisting existe !!! 
+/*
+
