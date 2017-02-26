e45c729f67ac3f012cb8825319d82b20b6a8182a
Ricardolv <richardluizv@gmail.com>
2017-02-26 01:17:23 -0300

    Operadores aritméticos

---
+++ simple/arithmetic_operators.js
@@ -0,0 +1,14 @@
+var temp = 4 + 3;
+console.log(temp);
+
+var temp = 25 - 3;
+console.log(temp);
+
+var temp = 30 / 2;
+console.log(temp);
+
+var temp = 30 * 3;
+console.log(temp);
+
+var temp = 7 % 2;
+console.log(temp);
