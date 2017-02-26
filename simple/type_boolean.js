28671ed634d1e46bfca480b809405e073f8fcc93
Ricardolv <richardluizv@gmail.com>
2017-02-26 01:03:06 -0300

    tipo boolean

---
+++ simple/type_boolean.js
@@ -0,0 +1,20 @@
+var ativo = true;
+console.log('tipo ', typeof ativo, ativo);
+
+// 0 e false
+console.log(!0);
+
+// o que 0 zero representa false em bollean
+console.log(!!0);
+
+// string vazia representa false em bollean
+console.log(!!'');
+
+// NaN  representa false em bollean
+console.log(!!NaN);
+
+// null  representa false em bollean
+console.log(!!null);
+
+// undefined  representa false em bollean
+console.log(!!undefined);
