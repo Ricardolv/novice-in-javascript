f1a56826ae4bb5ebf6fa2774d51d5dde8ae7ab4f
Ricardolv <richardluizv@gmail.com>
2017-02-26 01:13:33 -0300

    O operador typeof

---
+++ simple/the_typeof_operator.js
@@ -0,0 +1,23 @@
+var temp = 'teste';
+console.log('tipo ', typeof temp);
+
+temp = true;
+console.log('tipo ', typeof temp);
+
+temp = {};
+console.log('tipo ', typeof temp);
+
+temp = null;
+console.log('tipo ', typeof temp);
+
+temp = 99;
+console.log('tipo ', typeof temp);
+
+temp = undefined;
+console.log('tipo ', typeof temp);
+
+temp = [];
+console.log('tipo ', typeof temp);
+
+temp = function() {};
+console.log('tipo ', typeof temp);
