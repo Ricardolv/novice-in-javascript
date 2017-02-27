561bb59cc9bfd6d8c9951012b94bf7855e9aa25e
Ricardolv <richardluizv@gmail.com>
2017-02-27 08:46:14 -0300

    Operadores de comparacao

---
+++ simple/comparison_operators.js
@@ -0,0 +1,13 @@
+console.log(10 > 4);
+console.log(10 < 4);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+console.log(10 == 10);
+console.log('teste' == 'teste');
+console.log(10 != '10');
+
+//compra o valor e o tipo
+console.log(10 === '10');
+console.log(10 !== '10');
