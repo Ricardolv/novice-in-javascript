2961549e71f7ee8f91eb9ff9f81d5130397cd819
Ricardolv <richardluizv@gmail.com>
2017-02-27 15:43:38 -0300

    Array vs Object

---
+++ arrays/array_vs_object.js
@@ -0,0 +1,15 @@
+var foxArray = ['Fox', 'Prata', 'VW'];
+
+console.log(typeof foxArray)
+console.log(foxArray[0])
+console.log(foxArray[1])
+console.log(foxArray[2])
+
+console.log('================================================')
+
+var foxObject = {modelo: 'Fox', cor: 'Prata', fabricante: 'VW'}
+
+console.log(typeof foxObject)
+console.log(foxObject.modelo)
+console.log(foxObject.cor)
+console.log(foxObject.fabricante)
---
+++ arrays/introduction.js
@@ -0,0 +1,15 @@
+//criando array
+var emails = [];
+emails[0] = 'bernardo@email.com';
+emails[1] = 'hadassa@email.com';
+
+console.log('emails[0]', emails[0]);
+console.log('emails[5]', emails[5]);
+
+var nomes = ['Bernardo', 'Hadassa'];
+console.log('Quantos nomes?', nomes.length);
+
+nomes[4] = 'José';
+
+console.log('Quantos nomes depois do José?', nomes.length);
+console.log('nomes[3]', nomes[3]);
