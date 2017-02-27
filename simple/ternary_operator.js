07ca0be701810d556113a66e7ce13c28fe9a0f3e
Ricardolv <richardluizv@gmail.com>
2017-02-27 09:16:07 -0300

    Operador ternario

---
+++ simple/ternary_operator.js
@@ -0,0 +1,10 @@
+var temp = true;
+console.log(temp ? 'temp verdadeiro' : 'temp falso');
+
+var temFoto = true;
+// se tem fot : perfil.jpg
+// se nao tem foto: moc-perfil.jpg
+// <img src="pathFoto">
+
+var pathFoto = temp ? 'perfil.jpg' : 'moc-perfil.jpg';
+console.log(pathFoto);
