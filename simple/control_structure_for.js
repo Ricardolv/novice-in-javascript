166648cb94ac1d1b899e531a12c1f8a7331984ae
Ricardolv <richardluizv@gmail.com>
2017-02-27 09:25:16 -0300

    Estrutura de controle for

---
+++ simple/control_structure_for.js
@@ -0,0 +1,19 @@
+for (var i = 0 ; i < 5; i++) {
+	console.log('i', i);
+}
+
+var s = 'Paraguai';
+for (var J = 0; J < s.length; J++) {
+	console.log('J', J);
+}
+
+var K = 5;
+for (; K < 9; K++) {
+	console.log('K', K);
+}
+
+var y = 0;
+for (;y < 4;) {
+	console.log('y', y);
+	y++;
+}
