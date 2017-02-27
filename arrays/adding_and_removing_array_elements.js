60955900eb8419350dc5b0622df561b813572165
Ricardolv <richardluizv@gmail.com>
2017-02-27 15:50:50 -0300

    Adicionando e removendo elementos do array

---
+++ arrays/adding_and_removing_array_elements.js
@@ -0,0 +1,46 @@
+var emails = [];
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
+emails.push('bernardo@email.com');
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
+emails.push('hadassa@email.com');
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
+// adiciona na ultima posicao do elemento 
+emails.push('lala@email.com');
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
+// removendo o ultimo elemento do array
+emails.pop() 
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
+// adiciona na primeira posicao do elemento 
+emails.unshift('lala@email.com');
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
+// removendo na primeira posicao do elemento 
+emails.shift();
+
+console.log(emails);
+console.log(emails.length);
+console.log('=====================');
+
