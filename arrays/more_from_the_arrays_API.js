1ea06cdd343c94e465ea7187a3280c0305113901
Ricardolv <richardluizv@gmail.com>
2017-02-27 16:11:40 -0300

    Mais da API de arrays

---
+++ arrays/more_from_the_arrays_API.js
@@ -0,0 +1,7 @@
+var a = [];
+
+
+a.concat                a.copyWithin            a.entries               a.every                 a.fill                  a.filter                a.find
+a.findIndex             a.forEach               a.includes              a.indexOf               a.join                  a.keys                  a.lastIndexOf
+a.length                a.map                   a.pop                   a.push                  a.reduce                a.reduceRight           a.reverse
+a.shift                 a.slice                 a.some                  a.sort                  a.splice                a.unshift
